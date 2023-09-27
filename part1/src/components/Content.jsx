e80d4b52104f5df5b64f2bd3df9b19468b69d1aa
FiggyHunter <leonardo.roi3@gmail.com>
2023-09-27 17:25:33 +0200

    Completed Exercise 1.1

---
+++ part1/src/components/Content.jsx
@@ -0,0 +1,15 @@
+export default function Content(props) {
+  return (
+    <>
+      <p>
+        {props.parts[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercises[2]}
+      </p>
+    </>
+  );
+}
