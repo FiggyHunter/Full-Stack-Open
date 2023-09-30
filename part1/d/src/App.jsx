228f5ba006cbb6357368aab11860a6b34db85090
FiggyHunter <leonardo.roi3@gmail.com>
2023-09-30 22:32:47 +0200

    Completed Exercise 1.10

--- part1/d/src/App.jsx
+++ part1/d/src/App.jsx
@@ -6,12 +6,9 @@ const Button = ({ text, functionRef }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <tbody>
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    </tbody>
+    <p>
+      {text} {value}
+    </p>
   );
 };
 
