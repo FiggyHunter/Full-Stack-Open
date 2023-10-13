0ae301bcb1f8d62c4f144b5a9ba72f1da28ee242
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-13 11:39:25 +0200

    Completed Exercise 2.18

---
+++ part2/e/src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+
+function App() {
+  const [allCountries, setAllCountries] = useState({});
+  const [country, setCountry] = useState("");
+  const [displayedCountries, setDisplayedCountries] = useState({});
+
+  useEffect(() => {
+    Axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`).then(
+      (response) => {
+        setAllCountries(response.data);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(allCountries).length !== 0) {
+      filterCountries();
+    } // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [country]);
+
+  const handleChange = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const filterCountries = () => {
+    setDisplayedCountries(
+      allCountries.filter((searchedCountry) =>
+        searchedCountry.name.common
+          .toLowerCase()
+          .includes(country.toLowerCase())
+      )
+    );
+  };
+
+  return (
+    <>
+      <div className="country-finder">
+        <p>Find countries: </p>
+        <input value={country} onChange={handleChange}></input>
+        <h2>Countries:</h2>
+        <>
+          {displayedCountries.length > 1 &&
+            displayedCountries.length < 10 &&
+            displayedCountries?.map((country) => (
+              <p key={country.name.common}>{country.name.common}</p>
+            ))}
+
+          {displayedCountries.length === 1 &&
+            displayedCountries?.map((displayedCountry) => (
+              <div key={displayedCountry.name.common} className="country">
+                <h3>{displayedCountry.name.common}</h3>
+                <p>Capital City: {displayedCountry.capital}</p>
+                <h4>Languages:</h4>
+                {Object.values(displayedCountry.languages).map(
+                  (value, index) => (
+                    <ul key={index}>
+                      <li>{value}</li>
+                    </ul>
+                  )
+                )}
+                <img src={displayedCountry.flags.png}></img>
+              </div>
+            ))}
+
+          {displayedCountries.length > 10 && (
+            <p>Too many matches, please specify another filter! </p>
+          )}
+        </>
+      </div>
+    </>
+  );
+}
+
+export default App;
